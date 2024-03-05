71ca23fe37e9ce7e86e4cd7bc5b997b57b6aeeb3
Alicia Karla <karllynha.fontinelly@hotmail.com>
2024-03-05 19:54:25 -0300

    Numeros

---
+++ Exercicios/Numeros.py
@@ -0,0 +1,8 @@
+print('Tintas para pintar o quarto')
+
+altura = float(input('Altura da parede: '))
+largura = float(input('Qual a largura da parede: '))
+area = altura * largura
+tinta = area / 2
+
+print(f'É necessário {tinta} litros de tinta para pintar o quarto')
