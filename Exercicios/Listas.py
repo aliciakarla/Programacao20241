fa68dd3bc75604d3db8dbc5a931ac4537e3c2887
Alicia Karla <karllynha.fontinelly@hotmail.com>
2024-03-18 19:07:54 -0300

    LN

--- Exercicios/Listas.py
+++ Exercicios/Listas.py
@@ -1,6 +1,5 @@
 Soma_Num = 0
-for Num in range(3, 501): 
-    if Num % 2 != 3:  
+for Num in range(3, 501):  
         if Num % 3 == 0:  
             Soma_Num += Num
 print("A soma total é: {}.".format(Soma_Num))
