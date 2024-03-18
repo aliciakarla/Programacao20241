2c9fdc307626d5ce5255a7c90ae25fe7ae19044b
Alicia Karla <karllynha.fontinelly@hotmail.com>
2024-03-18 20:30:55 -0300

    Dics

---
+++ Exercicios/Dic2.py
@@ -0,0 +1,20 @@
+Dic1 = {}
+Dic2 = {}
+
+while True:
+    Nome = input('Digite um nome:')
+    Idade = (float(input('Idade: ')))
+    Soma = sum(Idade)
+    Media = Soma/len(Idade)
+
+    Continuar = input('Continuar? [S/N]: ').strip().upper()
+    while Continuar not in ('S', 'N'):
+        Continuar = input('Continuar? [S/N]: ').strip().upper()
+    if Continuar == 'N':
+        print('Finalizado...')
+        break
+    import random
+
+print (f'\nPessoas Cadastradas: {len(Nome)}')
+print (f'A idade média é: ', Media)
+print (f'A pessoa escolhida é: {random.choice(Nome)}')
