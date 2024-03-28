48278e769ca2a94f6cb572f7452d73f9a26af254
Alicia Karla <129808481+aliciakarla@users.noreply.github.com>
2024-03-27 22:37:12 -0300

    Atualizar o atv1q1.py

---
+++ Exercicios/atv1q1.py
@@ -0,0 +1,11 @@
+# Solicitar ao usuário para inserir as quatro notas
+nota1 = float(input("Insira a nota 1: "))
+nota2 = float(input("Insira a nota 2: "))
+nota3 = float(input("Insira a nota 3: "))
+nota4 = float(input("Insira a nota 4: "))
+
+# Calcular a média das notas
+media = (nota1 + nota2 + nota3 + nota4) / 4
+
+# Exibir a média final
+print("A média final é:", media)
