5f10ae898b3715223ad56cfdc56ad04bc62869ab
Alicia KArla <karllynhafontinelly@hotmail.com>
2024-09-02 20:30:09 -0300

    pro

--- Exercicios/Projeto.py
+++ Exercicios/Projeto.py
@@ -1,11 +1,31 @@
 import requests
 from bs4 import BeautifulSoup
 
-Link = "https://diatinf.ifrn.edu.br/docentes/"
-Requisicao = requests.get(Link)
 
-print(Requisicao)
-#print(Requisicao.text)
+def main():
+    Site = 'https://diatinf.ifrn.edu.br/docentes/'
+    Response = requests.get(Site)
+    Response.raise_for_status()
 
-Site = BeautifulSoup(Requisicao.text, "html.parser")
-#print(Site.prettify())
+    Soup = BeautifulSoup(Response.text, 'html.parser')
+    Professores = []
+
+    for Docente in Soup.select('.docente'):
+        Nome = Docente.select_one('.nome') 
+        Matricula = Docente.select_one('.matricula')
+        Email = Docente.select_one('.email') 
+
+        if Nome and Matricula and Email:
+            DadosProfessores= {
+                'nome': Nome.get_text(strip=True),
+                'matricula': Matricula.get_text(strip=True),
+                'email': Email.get_text(strip=True)
+            }
+            Professores.append(DadosProfessores)
+
+
+    for Professor in Professores:
+        print(f"Nome: {Professor['nome']}, Email: {Professor['email']}")
+
+if __name__ == '__main__':
+    main()
