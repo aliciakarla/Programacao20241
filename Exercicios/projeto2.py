4a044d4e324631c9a5293e977128243582162555
Alicia Karla <129808481+aliciakarla@users.noreply.github.com>
2024-09-12 16:07:27 -0300

    Update projeto2.py

--- Exercicios/projeto2.py
+++ Exercicios/projeto2.py
@@ -1,35 +1,33 @@
 import requests
 from bs4 import BeautifulSoup
 
-# URL da página com os dados dos professores
-url = "https://diatinf.ifrn.edu.br/docentes/"
+def main():
+    Link = 'https://diatinf.ifrn.edu.br/docentes/'
+    Response = requests.get(Link)
+    Response.raise_for_status()
+    print(Response)
+    #print(Response.text)
 
-# Enviar a requisição para obter o conteúdo da página
-response = requests.get(url)
-response.raise_for_status()  # Verifica se houve algum erro na requisição
+    Site = BeautifulSoup(Response.text, 'html.parser')
+    #print(Site.prettify())
+    Professores = []
 
-# Parsear o conteúdo HTML com BeautifulSoup
-soup = BeautifulSoup(response.text, 'html.parser')
+    for Docente in Site.select('.docente'):
+        Nome = Docente.select_one('h2') 
+        Matricula = Docente.select_one('h2')
+        Email = Docente.select_one('a') 
 
-# Lista para armazenar os dados dos professores
-professores = []
+        if Nome and Matricula and Email:
+            DadosProfessores= {
+                'h2': Nome.get_text(strip=True),
+                'h2': Matricula.get_text(strip=True),
+                'a': Email.get_text(strip=True)
+            }
+            Professores.append(DadosProfessores)
 
-# Encontrar todos os blocos que contêm as informações dos professores
-for docente in soup.select('.docente'):
-    nome = docente.select_one('.nome')
-    email = docente.select_one('.email')
-    
-    if nome and email:
-        # Limpar o texto e extrair informações
-        nome_texto = nome.get_text(strip=True)
-        email_texto = email.get_text(strip=True)
-        
-        # Adicionar os dados à lista de dicionários
-        professores.append({
-            'nome': nome_texto,
-            'email': email_texto
-        })
 
-# Imprimir os dados de nome e email de cada docente
-for professor in professores:
-    print(f"Nome: {professor['nome']}, Email: {professor['email']}")
+    for Professor in Professores:
+       print(f"Nome: {Professor['h2']}, Email: {Professor['a']}")
+
+if __name__ == '__main__':
+    main()
